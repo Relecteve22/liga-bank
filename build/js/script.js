f943ac628691aabec3887b0b66f68bfba04f9a2c
relecteve <Filonovaval@gmail.com>
2020-04-07 13:30:49 +0300

    Добавляет половина слайда

--- build/js/script.js
+++ build/js/script.js
@@ -108,6 +108,18 @@
 })();
 
 'use strict';
+$(document).ready(function () {
+  $('.list-promo').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 4000
+  });
+});
+
+'use strict';
 
 (function () {
   var linkNav = document.querySelectorAll('[href^="#"]');
---
+++ source/js/script-promo-slider.js
@@ -0,0 +1,11 @@
+'use strict';
+$(document).ready(function () {
+  $('.list-promo').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 4000
+  });
+});
