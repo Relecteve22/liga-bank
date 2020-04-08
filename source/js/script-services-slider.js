a11e978bb8fdea3f4cc93b812a2a105e95ed9e52
relecteve <Filonovaval@gmail.com>
2020-04-08 12:54:09 +0300

    Добавляет десктопную версию услуг

---
+++ source/js/script-services-slider.js
@@ -0,0 +1,34 @@
+'use strict';
+$(document).ready(function () {
+  var tabsList = document.querySelectorAll('.tabs-list li');
+  var arrayTabsList = Array.prototype.slice.call(tabsList, 0);
+  $('.list-services').slick({
+    arrows: false,
+    swipe: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+  arrayTabsList.forEach(function (btnTabs, i) {
+    btnTabs.addEventListener('click', function (evt) {
+      Array.prototype.forEach.call(tabsList, function (tab) {
+        if (tab.classList.contains('tabs-list__item--active')) {
+          tab.classList.remove('tabs-list__item--active');
+        }
+        if ((tab.querySelector('button')) === evt.target) {
+          tab.classList.add('tabs-list__item--active');
+        }
+        var arrayTabBtnChild = Array.prototype.slice.call(((tab.querySelector('button')).childNodes), 0);
+        arrayTabBtnChild.forEach(function (tabBtnChild, b) {
+          if (tabBtnChild === evt.target) {
+            tab.classList.add('tabs-list__item--active');
+          }
+        });
+        if ((tab.querySelector('svg use')) === evt.target) {
+          tab.classList.add('tabs-list__item--active');
+        }
+      });
+      $('.list-services').slick('slickGoTo', i);
+    });
+  });
+});
